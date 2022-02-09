2c38769050e14aea1f297b78ea826e4bb3046cdd
Code Hover <78866952+MakhdoomUsman@users.noreply.github.com>
2022-02-10 01:37:42 +0500

    updated portfolio
    Code with harry

---
+++ index.js
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>Muhammad Usman - Web Developer, UI Designer</title>
+    <link rel="stylesheet" href="./style.css" />
+  </head>
+  <body>
+    <div class="container">
+      <div class="sidebar">
+        <nav>
+          <li> <a href="./"></a> Home</li>
+          <li> <a href="./intro.html"></a> My intro</li>
+          <li> <a href="./services.html"></a> Services</li>
+          <li> <a href="./blog.html"></a> Blog</li>
+          <li> <a href="./cnotact.html"></a> Contact me</li>
+        </nav>
+      </div>
+      <div class="main">
+        <div class="infocontainer">
+          <div class="devinfo">
+            <div class="hello">Hello i am</div>
+            <div class="name">Muhamamd Usman</div>
+            <div class="about">Developer</div>
+            <div class="moreabout">Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusantium pariatur deserunt aspernatur in tempore sequi fugiat maxime, reiciendis quia corporis?</div>
+            <div class="button">
+                <button>Download CV</button>
+                <button>Contact me</button>
+            </div>
+          </div>
+          <div class="devpic"><img src="./usman.jpeg" alt="Usman" ></img></div>
+        </div>
+
+      </div>
+    </div>
+  </body>
+</html>
